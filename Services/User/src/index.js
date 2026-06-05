d9ea607a0461f91832ed050e59133ba162850dfa
Theo-th <theo.thivillier@viacesi.fr>
2026-06-05 23:00:48 +0200

    feat: init suite

---
+++ Services/User/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route de test qui sera appelée via la Gateway
+app.get('/', (req, res) => {
+  res.send('[User Service] : Démarrage...');
+});
+
+app.listen(port, () => {
+  console.log(`Service User démarré sur le port ${port}`);
+});
