6acb6daaa88c4fd654230d3b65f5a7126e84eb37
Theo-th <theo.thivillier@viacesi.fr>
2026-06-08 14:13:05 +0200

    feat: Création du composant navbar mobile

--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavbarMobile from "@/components/NavbarMobile";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col pb-16 md:pb-0 bg-white text-gray-900">
+        {children}
+        <NavbarMobile />
+      </body>
     </html>
   );
 }
