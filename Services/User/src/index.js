a226e8589297c4fa32fd66e660ae3ae730665b31
AntoninMignotPilon <antonin.mignotpilon@viacesi.fr>
2026-06-08 10:08:44 +0200

    add Morgan

--- Services/User/src/index.js
+++ Services/User/src/index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API User Service',
+      version: '1.0.0',
+      description: 'Documentation du service d\'User',
+    },
+    servers: [
+      {
+        url: 'http://localhost/api/v1/user',
+      },
+    ],
+  },
+  apis: ['./*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Route de test qui sera appelée via la Gateway
 app.get('/', (req, res) => {
