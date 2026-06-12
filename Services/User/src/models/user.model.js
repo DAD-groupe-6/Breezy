cba8994683a2c5c0b167c58ef1f9de505d86b958
AntoninMignotPilon <antonin.mignotpilon@viacesi.fr>
2026-06-12 10:09:26 +0200

    add user model

---
+++ Services/User/src/models/user.model.js
@@ -0,0 +1,55 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database.config");
+
+const User = sequelize.define("User", {
+    id_user: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false,
+    },
+    pseudo_uniq: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    pseudo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    bio: {
+        type: DataTypes.TEXT,
+    },
+    img_profile: {
+        type: DataTypes.STRING,
+    },
+    signalement: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+    nb_followers: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+    },
+}, {
+    timestamps: true,
+});
+
+const Follow = sequelize.define("Follow", {}, {
+    timestamps: true,
+});
+
+User.belongsToMany(User, {
+    through: Follow,
+    as: "Following",
+    foreignKey: "follower_id",
+    otherKey: "following_id",
+});
+
+User.belongsToMany(User, {
+    through: Follow,
+    as: "Followers",
+    foreignKey: "following_id",
+    otherKey: "follower_id",
+});
+
+module.exports = { User, Follow };
